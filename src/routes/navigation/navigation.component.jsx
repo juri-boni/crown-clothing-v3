c60a5c6b14cf33a4cc465ee9b85c743e1dff780d
juri-boni <100702839+juri-boni@users.noreply.github.com>
2022-12-17 15:29:32 +0100

    creating CartContext and implementing toggle cart functionality

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,7 +3,10 @@ import { Outlet, Link } from "react-router-dom";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
-import CartIcon from "../../assets/cart-icon/cart-icon.component";
+import { CartContext } from "../../contexts/cart.context";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
@@ -11,6 +14,7 @@ import "./navigation.styles.scss";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -33,6 +37,7 @@ const Navigation = () => {
           )}
           <CartIcon></CartIcon>
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
