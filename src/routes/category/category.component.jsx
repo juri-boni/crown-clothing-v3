79475e94e5497d73b8fb5c02f925e18cc9e9ce94
juri-boni <100702839+juri-boni@users.noreply.github.com>
2022-12-21 10:45:00 +0100

    fixing styling clashing and adding links to categories in shop component

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, Fragment } from "react";
 
 import { CategoriesContext } from "../../contexts/categories.context";
 
@@ -17,15 +17,18 @@ const Category = () => {
   }, [category, categoriesMap]);
 
   return (
-    <div className="category-container">
-      {
-        //use safeguard(products &&) to render the component only when data is available
-        products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))
-      }
-    </div>
+    <Fragment>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      <div className="category-container">
+        {
+          //use safeguard(products &&) to render the component only when data is available
+          products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))
+        }
+      </div>
+    </Fragment>
   );
 };
 
