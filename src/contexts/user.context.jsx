10ff96502487120caaa1c37ab86697c53f1b59ba
juri-boni <100702839+juri-boni@users.noreply.github.com>
2022-12-23 11:38:00 +0100

    updating user state management using reducers

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 import {
   onAuthStateChangedListener,
@@ -14,11 +14,58 @@ export const UserContext = createContext({
   setUserName: () => {},
 });
 
+/*************************************/
+/*************************************/
+
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+  SET_USER_NAME: "SET_USER_NAME",
+};
+
+const userReducer = (state, action) => {
+  console.log("dispatched");
+  console.log(action);
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    case USER_ACTION_TYPES.SET_USER_NAME:
+      return {
+        ...state,
+        userName: payload,
+      };
+
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+  userName: "",
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const [userName, setUserName] = useState("");
+  // const [currentUser, setCurrentUser] = useState(null);
+  // const [userName, setUserName] = useState("");
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  const { currentUser, userName } = state;
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
+  const setUserName = (userName) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_USER_NAME, payload: userName });
+  };
+
   const value = { currentUser, setCurrentUser, userName };
-  // console.log(currentUser);
+  console.log(currentUser);
+  console.log(`User Name: ${userName}`);
 
   useEffect(() => {
     const getUserData = async (user) => {
