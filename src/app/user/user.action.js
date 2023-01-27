57e50747c6a034bbd9c3e0e1f6c8932dd57e4811
juri-boni <100702839+juri-boni@users.noreply.github.com>
2023-01-27 14:39:30 +0100

    migration -- finish migrate user context to redux

--- src/app/user/user.action.js
+++ src/app/user/user.action.js
@@ -1,1 +1,10 @@
 import { USER_ACTION_TYPES } from "./user.types";
+import { createAction } from "../../utils/reducers/reducers.utils";
+
+export const setCurrentUser = (user) => {
+  createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
+};
+
+export const setUserName = (userName) => {
+  createAction(USER_ACTION_TYPES.SET_USER_NAME, userName);
+};
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import App from "./App";
-import { UserProvider } from "./contexts/user.context";
+// import { UserProvider } from "./contexts/user.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
 import { store } from "./app/store";
@@ -16,13 +16,13 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <UserProvider>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
-        </UserProvider>
+        {/* <UserProvider> */}
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
+        {/* </UserProvider> */}
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
