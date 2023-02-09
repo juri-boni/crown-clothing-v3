cb256f517fefc5f056f316006d6ff4134ae9fcb4
juri-boni <100702839+juri-boni@users.noreply.github.com>
2023-02-09 15:27:54 +0100

    fix -- cart.selector, app working, finished migration to redux

--- src/app/cart/cart.action.js
+++ src/app/cart/cart.action.js
@@ -2,21 +2,20 @@ import { CART_ACTION_TYPES } from "./cart.types";
 import { createAction } from "../../utils/reducers/reducers.utils";
 import { addCartItem, removeCartItem, clearCartItem } from "./cart.utilities";
 
-export const setIsCartOpen = (bool) => {
+export const setIsCartOpen = (bool) =>
   createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, bool);
-};
 
 export const addItemToCart = (cartItems, productToAdd) => {
   const newCartItems = addCartItem(cartItems, productToAdd);
-  createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+  return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
 };
 
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
   const newCartItems = removeCartItem(cartItems, cartItemToRemove);
-  createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+  return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
 };
 
 export const clearItemFromCart = (cartItems, cartItemToClear) => {
   const newCartItems = clearCartItem(cartItems, cartItemToClear);
-  createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+  return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
 };
