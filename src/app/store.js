d5f6585b55c6348ec0727eab3a0ba29eb4cc95fd
juri-boni <100702839+juri-boni@users.noreply.github.com>
2023-03-18 20:41:50 +0100

    setup -- store and rootreducer with redux toolkit

--- src/app/store.js
+++ src/app/store.js
@@ -1,41 +1,62 @@
-import {
-  compose,
-  legacy_createStore as createStore,
-  applyMiddleware,
-} from "redux";
+/******************************************************************************* */
+//REDUX WITH TOOLKIT
+/******************************************************************************* */
 
-import { persistStore, persistReducer } from "redux-persist";
+import { configureStore } from "@reactjs/toolkit";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
-
 import { rootReducer } from "./root-reducer";
-import storage from "redux-persist/lib/storage";
 
-const persistConfig = {
-  key: "root",
-  storage,
-  blacklist: ["user"],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const middlewares = [
-  process.env.NODE_ENV === "development" && logger,
-  thunk,
-].filter(Boolean);
-
-const composeEnhancer =
-  (process.env.NODE_ENV !== "production" &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
-
-const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
-
-export const store = createStore(
-  persistedReducer,
-  undefined,
-  composedEnhancers
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+  Boolean
 );
 
-export const persistor = persistStore(store);
+export const store = configureStore({
+  reducer: rootReducer,
+  // middleware: middleWares,
+});
+
+/******************************************************************************* */
+//REDUX WITHOUT TOOLKIT
+/******************************************************************************* */
+
+// import {
+//   compose,
+//   legacy_createStore as createStore,
+//   applyMiddleware,
+// } from "redux";
+
+// import { persistStore, persistReducer } from "redux-persist";
+// import logger from "redux-logger";
+// import thunk from "redux-thunk";
+
+// import { rootReducer } from "./root-reducer";
+// import storage from "redux-persist/lib/storage";
+
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   blacklist: ["user"],
+// };
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const middlewares = [
+//   process.env.NODE_ENV === "development" && logger,
+//   thunk,
+// ].filter(Boolean);
+
+// const composeEnhancer =
+//   (process.env.NODE_ENV !== "production" &&
+//     window &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose;
+
+// const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
+
+// export const store = createStore(
+//   persistedReducer,
+//   undefined,
+//   composedEnhancers
+// );
+
+// export const persistor = persistStore(store);
